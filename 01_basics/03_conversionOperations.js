af8b525b7d1903fd255cea41edb8dff8522a4c3e
Pushkar Pratap Singh <pushkarpratapsingh18@gmail.com>
2026-06-15 16:14:44 +0000

    container config done

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,23 @@
+let score=33
+console.log(typeof score);
+
+let score1= "33"
+console.log(typeof score1)
+
+let score2= "33abc" //NaN
+let score2= null //0
+let score2= undefined //NaN
+console.log(typeof score2);
+let valueInNumber=Number(score2)
+console.log(typeof valueInNumber);//conversion into number
+console.log(valueInNumber);//not a number
+ 
+
+//console.log(typeof score3);
+ 
+let isLoggedIn= 1 //output:true
+let isLoggedIn= ""//output: false
+let isLoggedIn="Ram"//output:true
+console.log(Boolean(isLoggedIn));
+ 
+ 
