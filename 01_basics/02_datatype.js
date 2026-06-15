a29e6e617c809fdf0bab01a0463c07ab15fb864f
Pushkar Pratap Singh <pushkarpratapsingh18@gmail.com>
2026-06-15 15:01:45 +0000

    container config done

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,10 @@
+  //data-types
+  let name ="pushkar"
+  let age =18
+  let isLoggedIn=false
+
+  console.log(typeof "Ram");
+console.log(typeof null);//important for interview
+console.log(typeof age);
+console.log(typeof undefined);
+
