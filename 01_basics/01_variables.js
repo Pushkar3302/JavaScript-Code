44aaee35dcf3e389cbc05ccadb94c522b0f66a7f
Pushkar Pratap Singh <pushkarpratapsingh18@gmail.com>
2026-06-15 14:42:14 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId= 144553
+let accountEmail = "push@google.com"
+var accountPassword="12345"// not to use 
+accountCity="Ghaziabad"
+  
+// accountEmail="utkarsh@gmail.com"
+// accountPassword="3301"
+// accountCity="Delhi"
+// accountId=123456
+let accountState;
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState]);
