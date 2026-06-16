90c2aca1e125d80addf925adeb9f1b0ddeb90f93
Pushkar Pratap Singh <pushkarpratapsingh18@gmail.com>
2026-06-16 06:07:05 +0000

    conversion of a operation

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -4,20 +4,39 @@ console.log(typeof score);
 let score1= "33"
 console.log(typeof score1)
 
-let score2= "33abc" //NaN
-let score2= null //0
-let score2= undefined //NaN
-console.log(typeof score2);
-let valueInNumber=Number(score2)
-console.log(typeof valueInNumber);//conversion into number
-console.log(valueInNumber);//not a number
+// let score2= "33abc" //NaN
+// let score2= null //0
+// let score2= undefined //NaN
+// console.log(typeof score2);
+//let valueInNumber=Number(score2)
+//console.log(typeof valueInNumber);//conversion into number
+//console.log(valueInNumber);//not a number
  
 
 //console.log(typeof score3);
  
-let isLoggedIn= 1 //output:true
-let isLoggedIn= ""//output: false
+// let isLoggedIn= 1 //output:true
+// let isLoggedIn= ""//output: false
 let isLoggedIn="Ram"//output:true
 console.log(Boolean(isLoggedIn));
  
  
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+
+
+// console.log( (3 + 4) * 5 % 3);
+
+
+let str1 = "hello"
+let str2 = " Ram"
+
+let str3 = str1 + str2
+ console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
